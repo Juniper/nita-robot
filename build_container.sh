2f00e330be5162a886a80c3679f31805bb430868
Matthew Tighe <matthew.e.tighe@gmail.com>
2026-04-01 11:57:31 -0700

    Added github workflow for robot container

--- build_container.sh
+++ build_container.sh
@@ -14,4 +14,4 @@
 #
 # ********************************************************
 
-docker build -t juniper/nita-robot:22.8-2 .
+docker build -t juniper/nita-robot:$(tr -d '\r\n[:space:]' < VERSION.txt) .
